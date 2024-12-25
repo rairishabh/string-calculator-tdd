9508471ef6942c3a8ed18015c2f29f1f42a12ac7
rairishabh <mailmerairishabh99@gmail.com>
2024-12-25 10:56:44 +0530

    [Test] test for negative number

--- src/test/StrignCalculator.test.js
+++ src/test/StrignCalculator.test.js
@@ -23,34 +23,45 @@ describe('String Calculator', () => {
     });
   });
 
-    // ---------Test for Any Amount Of Number--------
-    describe('Multiple Numbers', () => {
-      test('handle any amount of numbers', () => {
-        expect(calculator.add("1,2,3,4,5")).toBe(15);
-        expect(calculator.add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
-      });
+  // ---------Test for Any Amount Of Number--------
+  describe('Multiple Numbers', () => {
+    test('handle any amount of numbers', () => {
+      expect(calculator.add("1,2,3,4,5")).toBe(15);
+      expect(calculator.add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
+    });
+  });
+
+  // -------------Test For New Line-----------------
+  describe('New Line Handling', () => {
+    test('handle new lines between numbers', () => {
+      expect(calculator.add("1\n2,3")).toBe(6);
+      expect(calculator.add("1,2\n3")).toBe(6);
+      expect(calculator.add("1\n2\n3")).toBe(6);
+    });
+  });
+
+  // ------Support different delimiters--------
+  describe('Custom Delimiters', () => {
+    test('support custom delimiter with //', () => {
+      expect(calculator.add("//;\n1;2")).toBe(3);
+      expect(calculator.add("//$\n1$2$3")).toBe(6);
+      expect(calculator.add("//@\n2@3@4")).toBe(9);
     });
 
-    // -------------Test For New Line-----------------
-    describe('New Line Handling', () => {
-      test('handle new lines between numbers', () => {
-        expect(calculator.add("1\n2,3")).toBe(6);
-        expect(calculator.add("1,2\n3")).toBe(6);
-        expect(calculator.add("1\n2\n3")).toBe(6);
-      });
+    test('still support default delimiters with custom delimiter', () => {
+      expect(calculator.add("//;\n1;2\n3")).toBe(6);
+      expect(calculator.add("//;\n1;2,3")).toBe(6);
     });
-    
-    // ------Support different delimiters--------
-    describe('Custom Delimiters', () => {
-      test('support custom delimiter with //', () => {
-        expect(calculator.add("//;\n1;2")).toBe(3);
-        expect(calculator.add("//$\n1$2$3")).toBe(6);
-        expect(calculator.add("//@\n2@3@4")).toBe(9);
-      });
-  
-      test('still support default delimiters with custom delimiter', () => {
-        expect(calculator.add("//;\n1;2\n3")).toBe(6);
-        expect(calculator.add("//;\n1;2,3")).toBe(6);
-      });
+  });
+
+  // --------Test For Negative number--------
+  describe('Negative Numbers', () => {
+    test('throw error for single negative number', () => {
+      expect(() => calculator.add("-1,2")).toThrow("negative numbers not allowed: -1");
     });
+
+    test('throw error with all negative numbers in message', () => {
+      expect(() => calculator.add("2,-4,3,-5")).toThrow("negative numbers not allowed: -4,-5");
+    });
+  });
 });
