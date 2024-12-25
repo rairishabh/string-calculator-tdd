3c23ed93042ff108c73ca3eea3c44c35f3977242
rairishabh <mailmerairishabh99@gmail.com>
2024-12-25 10:36:48 +0530

    [Test] Added test for new line

--- src/test/StrignCalculator.test.js
+++ src/test/StrignCalculator.test.js
@@ -30,4 +30,13 @@ describe('String Calculator', () => {
         expect(calculator.add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
       });
     });
+
+    // -------------Test For New Line-----------------
+    describe('New Line Handling', () => {
+      test('handle new lines between numbers', () => {
+        expect(calculator.add("1\n2,3")).toBe(6);
+        expect(calculator.add("1,2\n3")).toBe(6);
+        expect(calculator.add("1\n2\n3")).toBe(6);
+      });
+    });
 });
